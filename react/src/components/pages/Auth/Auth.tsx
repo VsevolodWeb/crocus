2d5e2309ea437e943d96085babce2d130ff66df6
Vsevolod Ivanov <fromdaytoday@icloud.com>
2020-04-12 18:46:58 +0300

    register form

--- react/src/components/pages/Auth/Auth.tsx
+++ react/src/components/pages/Auth/Auth.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Form, Input, Button, Checkbox, Col, Row, Typography} from 'antd';
+import React, {useState} from 'react';
+import {Form, Input, Button, Checkbox, Col, Row, Typography, Spin} from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import {useHttp} from "../../../hooks/http.hook";
 
@@ -8,25 +8,23 @@ const { Title } = Typography;
 export const Auth = () => {
 	const {loading, error, request} = useHttp();
 
-	const registerHandler = async () => {
+	const registerHandler = async (values: any) => {
 		try {
-			const data = await request('/api/auth/register');
+			const data = await request('/api/auth/register', 'POST', {...values});
+			console.log(data);
 		} catch(e) {}
-	}
-
-	const onFinish = (values: any) => {
-		console.log('Received values of form: ', values);
 	};
 
 	return <>
 		<Title>Войти личный кабинет</Title>
-		<Row>
+		<Spin spinning={loading}>
+			<Row>
 			<Col span={6}>
 				<Form
 					name="normal_login"
 					className="login-form"
 					initialValues={{ remember: true }}
-					onFinish={onFinish}
+					onFinish={registerHandler}
 				>
 					<Form.Item
 						name="username"
@@ -44,27 +42,16 @@ export const Auth = () => {
 							placeholder="Password"
 						/>
 					</Form.Item>
-					<Form.Item>
-						<Form.Item name="remember" valuePropName="checked" noStyle>
-							<Checkbox>Запомнить меня</Checkbox>
-						</Form.Item>
-
-						<a className="login-form-forgot" href="!#">
-							Forgot password
-						</a>
-					</Form.Item>
 
 					<Form.Item>
 						<Button type="primary" htmlType="submit" className="login-form-button">
-							Log in
+							Register
 						</Button>
-						<div style={{marginTop: 20}}>
-							Or <a href="!#">register now!</a>
-						</div>
 					</Form.Item>
 				</Form>
 			</Col>
 		</Row>
+		</Spin>
 	</>
 };
 
