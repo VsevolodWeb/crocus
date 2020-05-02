34cbebba6c3d5fcb7589157a1643b6c2cdfb19a1
Vsevolod Ivanov <fromdaytoday@icloud.com>
2020-05-02 23:36:34 +0300

    burger menu & slider fix

--- react/src/site/Site.tsx
+++ react/src/site/Site.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './styles/main.sass'
 import Main from "./pages/Main/Main";
 import Icons from "./components/Icons/Icons";
+import {slide as Menu} from "react-burger-menu";
 
 const Site = () => {
 	return (
 		<>
+			<Menu right>
+				<a id="home" className="menu-item" href="/">Home</a>
+				<a id="about" className="menu-item" href="/about">About</a>
+				<a id="contact" className="menu-item" href="/contact">Contact</a>
+			</Menu>
 			<Main/>
 			<Icons/>
 		</>
